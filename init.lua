f0418ebd9d18441e7959a93627c19f063f7dc061
Asbjørn Apeland <aude@users.noreply.github.com>
2020-10-30 12:27:08 +0100

    search line by line
    fixes #2

--- init.lua
+++ init.lua
@@ -1,27 +1,27 @@
 function mark_trailing_whitespace(win)
 	-- search through visible area for trailing whitespace
 	local content = win.file:content(win.viewport)
+
+	-- remember position in file
 	local offset = win.viewport.start
-	local search_from = 0
-	while true do
+
+	for line in string.gmatch(content, "([^\n]*)") do
 		-- search for trailing whitespace
 		-- note: string.find() returns 1-based index, not 0-based
-		local from, to = string.find(content, '%s+%f[\n]', search_from)
+		local from, to = string.find(line, '%s+$')
 
-		-- stop searching if no more matches found
-		if from == nil then
-			break
+		-- if match found
+		if from ~= nil then
+			-- mark
+			-- convert from 1-based index to 0-based
+			-- also add the offset
+			local mark_from = from - 1 + offset
+			local mark_to = to - 1 + offset
+			win:style(win.STYLE_CURSOR, mark_from, mark_to)
 		end
 
-		-- mark
-		-- convert from 1-based index to 0-based
-		-- also add the offset
-		local mark_from = from - 1 + offset
-		local mark_to = to - 1 + offset
-		win:style(win.STYLE_CURSOR, mark_from, mark_to)
-
-		-- start next search after the current match
-		search_from = to + 1
+		-- +1 to count the '\n'
+		offset = offset + string.len(line)+1
 	end
 end
 
